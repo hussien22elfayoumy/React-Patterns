c3a3d8a0da69050895c9b56892b85857b0d2b7c4
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-06-26 19:33:31 +0300

    workin with debouncing

--- src/components/SearchableList/searchable-list.jsx
+++ src/components/SearchableList/searchable-list.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function SearchableList({ items, children, itemKeyFn }) {
   const [searchTerm, setSearchTerm] = useState('');
+  const lastChange = useRef();
 
   const searchResults = items.filter((item) =>
     JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   function handleChange(e) {
-    setSearchTerm(e.target.value);
+    // If a previous debounce timer is running, clear it
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    // Start a new debounce timer
+    lastChange.current = setTimeout(() => {
+      // Once 500ms have passed without further input, clear the timer reference
+      lastChange.current = null;
+
+      // Finally, update the search term state with the latest input
+      setSearchTerm(e.target.value);
+    }, 500);
   }
+
   return (
     <div className='searchable-list'>
       <input type='search' onChange={handleChange} placeholder='Search' />
